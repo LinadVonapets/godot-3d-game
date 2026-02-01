f4ba3e9a862af854d8083072658e4585cb726795
Daniil Stepanov <10022004d@gmail.com>
2026-02-02 00:34:53 +0300

    main: script: add score_label runtime class

---
+++ src/score_label.cpp
@@ -0,0 +1,15 @@
+#include "score_label.hpp"
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/array.hpp>
+
+using namespace godot;
+
+void ScoreLabel::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("_on_mob_squashed"), &ScoreLabel::_on_mob_squashed);
+}
+
+void ScoreLabel::_on_mob_squashed() {
+	this->score++;
+	set_text(String("Score: {0}").format(Array::make(this->score)));
+}
